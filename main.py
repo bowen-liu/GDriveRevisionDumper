9be68850b230ce27b479b61762d8c7f17f573eb1
Bowen <bin4ry@live.com>
2018-03-28 13:40:57 -0700

    Switched to API v2
    For what we're trying to accomplish, V2 is much more suitable than V3. V2 allows direct export of past Google Docs revision, and has the download/export urls directly in the JSON respond. V3 has none of those.

--- main.py
+++ main.py
@@ -27,34 +27,23 @@ GSLIDE_MIMETYPE = 'application/vnd.google-apps.presentation'
 
 #Setup an authenticated http and GDrive REST API object
 http = get_credentials().authorize(httplib2.Http())
-service = discovery.build('drive', 'v3', http=http)
+service = discovery.build('drive', 'v2', http=http)
 
 
 def main():
 
-    """
+
     #docrevtest
-    REQUEST_FILEID = '1aB1CFZ_KRdwabWIctMLqoimG3Fn1dnuBelKanLNjfP8'     
-    EXPORT_FORMAT = 'docx'
-    """
+    #REQUEST_FILEID = '1aB1CFZ_KRdwabWIctMLqoimG3Fn1dnuBelKanLNjfP8'     
 
-    """
     #sheetrevtest
-    REQUEST_FILEID = '1S5vUsWEmnV-uLb7-7m-jOdcBxAKo_834us9x56s7xnw'     
-    EXPORT_FORMAT = 'xlsx'
-    """
+    #REQUEST_FILEID = '1S5vUsWEmnV-uLb7-7m-jOdcBxAKo_834us9x56s7xnw'     
 
-    """
     #sliderevtest
-    REQUEST_FILEID = '1_f_abGgeI4CruG72Yjjicrce9rk0NRcc4_4vYjd5JNQ'     
-    EXPORT_FORMAT = 'pptx'
-    #https://docs.google.com/presentation/u/0/d/1_f_abGgeI4CruG72Yjjicrce9rk0NRcc4_4vYjd5JNQ/export?format=pptx&revision=1
-    """
+    #REQUEST_FILEID = '1_f_abGgeI4CruG72Yjjicrce9rk0NRcc4_4vYjd5JNQ'     
 
     #User file in google drive
     REQUEST_FILEID = '1tG6oBCtgMfqrXa34Rk1HWrudLMzEjZ84'
-    EXPORT_FORMAT = 'png'
-
 
     fileInfo = service.files().get(
         fileId = REQUEST_FILEID
@@ -63,16 +52,20 @@ def main():
     #Determine what type of file this is
     fileMimeType = fileInfo['mimeType']
     if fileMimeType == GDOC_MIMETYPE:
-        DOCTYPE = 'document'
+        export_extension = 'docx'
+        export_format = 'application/vnd.openxmlformats-officedocument.wordprocessingml.document'
     elif fileMimeType == GSHEET_MIMETYPE:
-        DOCTYPE = 'spreadsheets'
+        export_extension = 'xlsx'
+        export_format = 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'
     elif fileMimeType == GSLIDE_MIMETYPE:
-        DOCTYPE = 'presentation'
+        export_extension = 'pptx'
+        export_format = 'application/vnd.openxmlformats-officedocument.presentationml.presentation'
     else:
-        DOCTYPE = ''
+        export_extension = 'png'        #temporary
+        export_format = ''
     
     #Create a folder with the file's name as its download destination
-    fileName = fileInfo['name']
+    fileName = fileInfo['title']
     fileDLPath = os.path.join(os.getcwd(), fileName)
     if not os.path.exists(fileDLPath):
         os.makedirs(fileDLPath)
@@ -81,47 +74,46 @@ def main():
 
     #Begin downloading all revisions
     hasAllRevisions = False
-    result_nextPageToken = ''
+    result_pageToken = ''
 
     while not hasAllRevisions:
 
         #Call the Revision.List function to get a list of all revisions for the file
         #results is a dict containing the return fields specified in the API documentations
-        if result_nextPageToken is '':
+        #https://developers.google.com/drive/v2/reference/revisions/list
+        if result_pageToken is '':
             revResponse = service.revisions().list(
                 fileId = REQUEST_FILEID,
-                pageSize = 2
+                maxResults = 2
                 ).execute() 
         else:
              revResponse = service.revisions().list(
                 fileId = REQUEST_FILEID,
-                pageSize = 2,
-                pageToken = result_nextPageToken
+                maxResults = 2,
+                pageToken = result_pageToken
                 ).execute()
         #print(json.dumps(results, sort_keys=True, indent=4))
         
-        for rev in revResponse['revisions']:
-            print(rev['id'], rev['modifiedTime'])
+        for rev in revResponse['items']:
+            print(rev['id'], rev['modifiedDate'])
 
-            #Generic user file
-            if DOCTYPE is '':
-                pass
-            
-            #Google Docs related files
+            if not 'downloadUrl' in rev:
+                dl_url = rev['exportLinks'][export_format]          #file is from google doc 
             else:
-                dl_url = 'https://docs.google.com/{}/u/0/d/{}/export?format={}&revision={}'.format(DOCTYPE, REQUEST_FILEID , EXPORT_FORMAT, rev['id']) 
-                dl_response, dl_content = http.request(dl_url)
-                print(dl_response.status)
+                dl_url = rev['downloadUrl']                         #any other files in google drive
 
-                output_path = '{}\{}.{}'.format(fileDLPath, rev['id'], EXPORT_FORMAT)
-                with open(output_path, 'wb') as f:
-                    f.write(dl_content)
+            dl_response, dl_content = http.request(dl_url)
+            print(dl_response.status)
+
+            output_path = '{}\{}.{}'.format(fileDLPath, rev['id'], export_extension)
+            with open(output_path, 'wb') as f:
+                f.write(dl_content) 
 
         #Prepare for the next iteration, if there are still revision pages remaining to fetch
         if not 'nextPageToken' in revResponse:
             hasAllRevisions = True
         else:
-            result_nextPageToken = revResponse['nextPageToken']
+            result_pageToken = revResponse['nextPageToken']
 
 
 if __name__ == '__main__':
